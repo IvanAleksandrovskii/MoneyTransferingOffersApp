dfa0813897b123ab2da33c8bcf69b786408b1ab9
Ivan Aleksandrovskii <i.aleksandrovskii@chrona.ai>
2024-08-01 02:16:49 +0700

    UUID postgres fix

--- core/models/base.py
+++ core/models/base.py
@@ -1,6 +1,6 @@
 import uuid
 
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, text
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
 
@@ -19,4 +19,9 @@ class Base(DeclarativeBase):
     def __tablename__(cls) -> str:
         return f"{camel_case_to_snake_case(cls.__name__)}s"
 
-    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True)
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        server_default=text("gen_random_uuid()"),
+        default=uuid.uuid4
+    )
