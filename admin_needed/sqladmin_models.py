7896ceae2d2922cd3c0221898853714c02eee30e
Ivan Aleksandrovskii <i.aleksandrovskii@chrona.ai>
2024-08-07 00:55:55 +0700

    sqladmin improved

--- admin_needed/sqladmin_models.py
+++ admin_needed/sqladmin_models.py
@@ -13,27 +13,29 @@ def format_exchange_rate(model, name):
 
 
 def format_transfer_rule(model, name):
-    return f"{model.provider.name} - {model.send_country.name} - {model.receive_country.name} - {model.transfer_currency.abbreviation if model.transfer_currency else 'Unknown'} - {model.min_transfer_amount} - {model.max_transfer_amount}"
+    return (f"{model.provider.name} - {model.send_country.name} - {model.receive_country.name} - "
+            f"{model.transfer_currency.abbreviation if model.transfer_currency else 'Unknown'} - "
+            f"{model.min_transfer_amount} - {model.max_transfer_amount}")
 
 
 class CountryAdmin(ModelView, model=Country):
-    column_list = [Country.id, Country.name, Country.is_active, Country.local_currency_id]
+    column_list = [Country.name, Country.id, Country.is_active, Country.local_currency_id]
     name = "Country"
     name_plural = "Countries"
-    # category = "" # TODO: group by categories
+    category = "Global"  # Done
 
 
 class CurrencyAdmin(ModelView, model=Currency):
-    column_list = [Currency.id, Currency.abbreviation, Currency.is_active, Currency.name, Currency.symbol]
+    column_list = [Currency.abbreviation, Currency.id, Currency.is_active, Currency.name, Currency.symbol]
     name = "Currency"
     name_plural = "Currencies"
-    # category = "" # TODO: group by categories
+    category = "Global"  # Done
 
 
 class TransferProviderAdmin(ModelView, model=TransferProvider):
-    column_list = [TransferProvider.id, TransferProvider.name, TransferProvider.is_active]
+    column_list = [TransferProvider.name, TransferProvider.id, TransferProvider.is_active]
     form_excluded_columns = ['exchange_rates', 'transfer_rules']
-    # category = "" # TODO: group by categories
+    category = "Providers"  # Done
 
 
 class TransferRuleAdmin(ModelView, model=TransferRule):
@@ -41,34 +43,34 @@ class TransferRuleAdmin(ModelView, model=TransferRule):
         "formatted_transfer_rule",
         TransferRule.id,
         TransferRule.is_active,
+        TransferRule.fee_percentage,
+        TransferRule.min_transfer_amount,
+        TransferRule.max_transfer_amount,
         TransferRule.send_country_id,
         TransferRule.receive_country_id,
         TransferRule.transfer_currency_id,
-        TransferRule.provider_id,
-        TransferRule.fee_percentage,
-        TransferRule.min_transfer_amount,
-        TransferRule.max_transfer_amount
+        TransferRule.provider_id
     ]
     column_formatters = {
         "formatted_transfer_rule": format_transfer_rule
     }
     name = "Transfer Rule"
-    # category = "" # TODO: group by categories
+    category = "Providers"  # Done
 
 
 class ProviderExchangeRateAdmin(ModelView, model=ProviderExchangeRate):
     column_list = [
         "formatted_exchange_rate",
+        ProviderExchangeRate.rate,
         ProviderExchangeRate.id,
+        ProviderExchangeRate.last_updated,
         ProviderExchangeRate.is_active,
         ProviderExchangeRate.provider_id,
         ProviderExchangeRate.from_currency_id,
-        ProviderExchangeRate.to_currency_id,
-        ProviderExchangeRate.rate,
-        ProviderExchangeRate.last_updated
+        ProviderExchangeRate.to_currency_id
     ]
     column_formatters = {
         "formatted_exchange_rate": format_exchange_rate
     }
     name = "Provider Exchange Rate"
-    # category = "" # TODO: group by categories
+    category = "Providers"  # Done
