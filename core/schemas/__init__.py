a55ae76e00cd1ff7be767158068487aa18a06c9d
Ivan Aleksandrovskii <i.aleksandrovskii@chrona.ai>
2024-08-09 01:52:50 +0700

    imports update

--- core/schemas/__init__.py
+++ core/schemas/__init__.py
@@ -1,14 +1,11 @@
 __all__ = ["ProviderResponse", "CurrencyResponse", "CountryResponse", "ExchangeRateResponse",
-           "TransferRuleDetails", "GenericObjectResponse",
-           "DetailedTransferRuleResponse", "OptimizedTransferRuleResponse"]
+           "TransferRuleDetails", "GenericObjectResponse", "DetailedTransferRuleResponse",
+           "OptimizedTransferRuleResponse"]
 
-# "TransferRuleRequest", "TransferRuleRequestByName", "TransferRuleFullRequest"
 
 from .country import CountryResponse
 from .currency import CurrencyResponse
 from .provider import ProviderResponse
 from .transfer_rule import TransferRuleDetails, DetailedTransferRuleResponse, OptimizedTransferRuleResponse
 from .exchange_rate import ExchangeRateResponse
-# from .transfer_request import TransferRuleRequest
-# from .transfer_request import TransferRuleRequestByName, TransferRuleFullRequest
 from .generic_obj_response import GenericObjectResponse
