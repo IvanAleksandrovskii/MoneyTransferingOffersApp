ea480ff7522607435dad028c632b91557fb9fcac
Ivan Aleksandrovskii <i.aleksandrovskii@chrona.ai>
2024-08-09 04:45:42 +0700

    admin improved

--- core/admin/sqladmin_models.py
+++ core/admin/sqladmin_models.py
@@ -1,10 +1,16 @@
-from sqladmin import ModelView
+from sqladmin import ModelView, action
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import Session
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
 
+from core.admin.sqladmin_db_helper import sync_sqladmin_db_helper
 from core.models import Country
 from core.models import Currency
 from core.models import TransferProvider
 from core.models import TransferRule
 from core.models import ProviderExchangeRate
+from core import logger
 
 
 def format_exchange_rate(model, name):  # Do not delete name form here in is not unused (!), needed for formatting
@@ -17,32 +23,73 @@ def format_transfer_rule(model, name):  # Do not delete name form here in is not
             f"{model.min_transfer_amount} - {model.max_transfer_amount}")
 
 
-class CountryAdmin(ModelView, model=Country):
-    column_list = [Country.name, Country.id, Country.is_active, Country.local_currency_id]
+class BaseAdminModel(ModelView):
+    column_list = ['id', 'is_active']  # Base fields for all models
+
+    def _process_action(self, request: Request, is_active: bool) -> None:
+        pks = request.query_params.get("pks", "").split(",")
+        if pks:
+            try:
+                with Session(sync_sqladmin_db_helper.engine) as session:
+                    for pk in pks:
+                        model = session.get(self.model, pk)
+                        if model:
+                            model.is_active = is_active
+                    session.commit()
+                logger.info(f"Successfully {'activated' if is_active else 'deactivated'} {len(pks)} objects")
+            except SQLAlchemyError as e:
+                logger.error(f"An error occurred: {str(e)}")
+                session.rollback()
+
+    @action(
+        name="activate",
+        label="Activate",
+        confirmation_message="Are you sure you want to activate selected %(model)s?",
+        add_in_detail=True,
+        add_in_list=True,
+    )
+    def activate(self, request: Request) -> RedirectResponse:
+        self._process_action(request, True)
+        return RedirectResponse(request.url_for("admin:list", identity=self.identity), status_code=302)
+
+    @action(
+        name="deactivate",
+        label="Deactivate",
+        confirmation_message="Are you sure you want to deactivate selected %(model)s?",
+        add_in_detail=True,
+        add_in_list=True,
+    )
+    def deactivate(self, request: Request) -> RedirectResponse:
+        self._process_action(request, False)
+        return RedirectResponse(request.url_for("admin:list", identity=self.identity), status_code=302)
+
+
+class CountryAdmin(BaseAdminModel, model=Country):
+    column_list = BaseAdminModel.column_list + [Country.name, Country.local_currency_id]
     name = "Country"
     name_plural = "Countries"
-    category = "Global"  # Done todo double check
+    category = "Global"
+    can_delete = False
 
 
-class CurrencyAdmin(ModelView, model=Currency):
-    column_list = [Currency.abbreviation, Currency.id, Currency.is_active, Currency.name, Currency.symbol]
+class CurrencyAdmin(BaseAdminModel, model=Currency):
+    column_list = BaseAdminModel.column_list + [Currency.abbreviation, Currency.name, Currency.symbol]
     form_excluded_columns = ['countries']
     name = "Currency"
     name_plural = "Currencies"
-    category = "Global"  # Done todo double check
+    category = "Global"
+    can_delete = False
 
 
-class TransferProviderAdmin(ModelView, model=TransferProvider):
-    column_list = [TransferProvider.name, TransferProvider.id, TransferProvider.is_active, TransferProvider.url]
+class TransferProviderAdmin(BaseAdminModel, model=TransferProvider):
+    column_list = BaseAdminModel.column_list + [TransferProvider.name, TransferProvider.url]
     form_excluded_columns = ['exchange_rates', 'transfer_rules']
-    category = "Providers"  # Done todo double check
+    category = "Providers"
 
 
-class TransferRuleAdmin(ModelView, model=TransferRule):
-    column_list = [
+class TransferRuleAdmin(BaseAdminModel, model=TransferRule):
+    column_list = BaseAdminModel.column_list + [
         "formatted_transfer_rule",
-        TransferRule.id,
-        TransferRule.is_active,
         TransferRule.fee_percentage,
         TransferRule.min_transfer_amount,
         TransferRule.max_transfer_amount,
@@ -55,26 +102,22 @@ class TransferRuleAdmin(ModelView, model=TransferRule):
         "formatted_transfer_rule": format_transfer_rule
     }
     name = "Transfer Rule"
-    category = "Providers"  # Done todo double check
+    category = "Providers"
 
 
-class ProviderExchangeRateAdmin(ModelView, model=ProviderExchangeRate):
-    column_list = [
+class ProviderExchangeRateAdmin(BaseAdminModel, model=ProviderExchangeRate):
+    column_list = BaseAdminModel.column_list + [
         "formatted_exchange_rate",
         ProviderExchangeRate.rate,
-        ProviderExchangeRate.id,
         ProviderExchangeRate.last_updated,
-        ProviderExchangeRate.is_active,
         ProviderExchangeRate.provider_id,
         ProviderExchangeRate.from_currency_id,
         ProviderExchangeRate.to_currency_id
     ]
-    form_excluded_columns = [
-        "last_updated"
-    ]
+    form_excluded_columns = ["last_updated"]
     column_formatters = {
         "last_updated": lambda m, a: m.last_updated.strftime("%Y-%m-%d %H:%M:%S") if m.last_updated else "",
         "formatted_exchange_rate": format_exchange_rate
     }
     name = "Provider Exchange Rate"
-    category = "Providers"  # Done todo double check
+    category = "Providers"
