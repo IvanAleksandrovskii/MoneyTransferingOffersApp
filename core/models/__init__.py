563dc0b5e83d5405dc16ca4cd2303e8c4d13fe8b
Ivan Aleksandrovskii <i.aleksandrovskii@chrona.ai>
2024-08-10 02:39:53 +0700

    document model added

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,10 +1,12 @@
-__all__ = ["Base", "db_helper", "Currency", "Country", "TransferProvider",
+__all__ = ["Base", "db_helper", "Currency", "Country", "Document", "TransferProvider",
            "ProviderExchangeRate", "TransferRule"]
+
 
 from .base import Base
 from .db_helper import db_helper
 from .currency import Currency
 from .country import Country
+from .document import Document
 from .transfer_provider import TransferProvider
 from .provider_exchange_rate import ProviderExchangeRate
 from .transfer_rule import TransferRule
---
+++ core/models/document.py
@@ -0,0 +1,14 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from core.models import Base
+
+
+class Document(Base):
+    __tablename__ = "documents"
+
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    transfer_rules = relationship("TransferRule", secondary="transfer_rule_documents",
+                                  back_populates="required_documents", lazy="noload")
+
+    def __repr__(self) -> str:
+        return f"<Document(id={self.id}, name={self.name})>"
