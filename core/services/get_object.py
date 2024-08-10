4870861eab24952cd3077d1c61720719f8f43c80
Ivan Aleksandrovskii <i.aleksandrovskii@chrona.ai>
2024-08-10 15:02:31 +0700

    del unused code

--- core/services/get_object.py
+++ core/services/get_object.py
@@ -45,22 +45,3 @@ async def get_object_by_id(session: AsyncSession, model, _id: UUID):
         raise HTTPException(model.__name__, str(_id))
 
     return obj
-
-# async def get_object_by_id(session: AsyncSession, model, id: UUID):
-#     """
-#     Retrieve an active object by its ID.
-#     """
-#     query = model.active().filter(model.id == id)
-#
-#     if model == Country:
-#         query = query.options(joinedload(Country.local_currency))
-#
-#     result = await session.execute(query)
-#     obj = result.unique().scalar_one_or_none()
-#
-#     if obj:
-#         logger.info(f"Found active {model.__name__}: {obj.id}")
-#     else:
-#         logger.warning(f"Active {model.__name__} not found for id: {id}")
-#
-#     return obj
