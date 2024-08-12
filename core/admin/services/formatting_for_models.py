5398fe7dd892b88b9b144eb06bd30edd40a21c15
Ivan Aleksandrovskii <i.aleksandrovskii@chrona.ai>
2024-08-12 12:01:43 +0700

    delete unused formatter

--- core/admin/services/formatting_for_models.py
+++ core/admin/services/formatting_for_models.py
@@ -1,8 +1,2 @@
 def format_exchange_rate(model, name):
     return f"{model.provider.name} - {model.from_currency.abbreviation} - {model.to_currency.abbreviation}"
-
-
-# def format_transfer_rule(model, name):
-#     return (f"{model.provider.name} - {model.send_country.name} - {model.receive_country.name} - "
-#             f"{model.transfer_currency.abbreviation if model.transfer_currency else 'Unknown'} - "
-#             f"{model.min_transfer_amount} - {model.max_transfer_amount}")
