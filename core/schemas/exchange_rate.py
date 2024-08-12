47f8fd05e9fe62b8a5fd280e4c6b4465102b5b19
Ivan Aleksandrovskii <i.aleksandrovskii@chrona.ai>
2024-08-12 12:00:00 +0700

    validation added

--- core/schemas/exchange_rate.py
+++ core/schemas/exchange_rate.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from pydantic import Field, ConfigDict
+from pydantic import Field
 
 from .base import BaseResponse
 from .provider import ProviderResponse
@@ -14,15 +14,13 @@ class ExchangeRateResponse(BaseResponse):
     rate: float = Field(..., gt=0)
     last_updated: datetime
 
-    model_config = ConfigDict(from_attributes=True)
-
-    # @classmethod
-    # def model_validate(cls, obj):
-    #     return cls(
-    #         id=obj.id,
-    #         provider=ProviderResponse.model_validate(obj.provider),
-    #         from_currency=CurrencyResponse.model_validate(obj.from_currency),
-    #         to_currency=CurrencyResponse.model_validate(obj.to_currency),
-    #         rate=obj.rate,
-    #         last_updated=obj.last_updated
-    #     )
+    @classmethod
+    def model_validate(cls, obj, **kwargs):
+        return cls(
+            id=obj.id,
+            provider=ProviderResponse.model_validate(obj.provider),
+            from_currency=CurrencyResponse.model_validate(obj.from_currency),
+            to_currency=CurrencyResponse.model_validate(obj.to_currency),
+            rate=obj.rate,
+            last_updated=obj.last_updated
+        )
