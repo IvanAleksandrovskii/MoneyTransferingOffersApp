76a599633f61c008ec421ad18c743934144bde34
Ivan Aleksandrovskii <i.aleksandrovskii@chrona.ai>
2024-08-12 17:33:49 +0700

    super endpoint deleted

--- api/__init__.py
+++ api/__init__.py
@@ -3,12 +3,11 @@ from fastapi import APIRouter
 from .api_main_views import router as main_router
 from .api_views_global_objects import router as global_objects_router
 from .api_views_provider_objects import router as provider_objects_router
-from .api_super_views import router as super_router
 
 
 api_router = APIRouter()
 
 api_router.include_router(main_router, tags=["MAIN"])
-api_router.include_router(super_router, tags=["[WARNING: DEV TOOL, DELETE IT] Super [WARNING: VERY HIGH LOAD]"])
+
 api_router.include_router(global_objects_router, prefix="/global-objects", tags=["Global Objects"])
 api_router.include_router(provider_objects_router, prefix="/provider-objects", tags=["Provider Objects"])
